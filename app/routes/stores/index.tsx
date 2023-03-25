997e2292e73cc2d7bd330c3e49ddd1a8ae739056
lucianolivares <lucianolivares@gmail.com>
2023-03-25 00:26:35 -0300

    using storeslist on store route

--- app/routes/stores/index.tsx
+++ app/routes/stores/index.tsx
@@ -1,24 +1,11 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import type { Query } from "firebase-admin/firestore";
 import { auth } from "~/auth.server";
 import type { AuthUser } from "~/auth.server/auth-types";
-import { db } from "~/firebase";
+import StoresList from "~/components/StoresList";
+import type { StoreType} from "~/controllers.server/stores";
+import { getStores } from "~/controllers.server/stores";
 
-type StoreType = {
-  name: string;
-};
-
-const getStores = async (userId: string) => {
-  const storesRef = db.collection("users").doc(userId).collection("stores");
-  let query: Query = storesRef;
-  query = query.orderBy("name", "desc");
-
-  const stores = (await query.get()).docs.map((doc) => {
-    return { id: doc.id, ...doc.data() };
-  });
-  return stores;
-};
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user: AuthUser = await auth.user(request);
@@ -31,7 +18,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function StoresIndexRoute() {
   const data = useLoaderData();
-  const user = data.user;
+  const user: AuthUser = data.user;
   const stores: Array<StoreType> = data?.stores;
 
   return (
@@ -40,18 +27,7 @@ export default function StoresIndexRoute() {
         Stores de {user.username} 📚
       </h1>
       <main className="flex flex-col">
-        {/* Crear componente lista de stores */}
-        <ul className="flex flex-col mb-4 gap-2">
-          {!stores.length && <h1>No has creado ninguna lista</h1>}
-          {stores.map((store, index: any) => (
-            <li
-              key={index}
-              className="dark:text-white self-center bg-gray-700 p-2"
-            >
-              <Link to={store.name}>{store.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <StoresList stores={stores} />
         <Link className="rounded-full m-auto p-4 bg-teal-500" to="new-store">
           Nuevo Store
         </Link>
