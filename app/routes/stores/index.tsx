31a0910786adab1a76161fda6e7ebbc3e06a53d7
lucianolivares <lucianolivares@gmail.com>
2023-03-24 22:20:09 -0300

    update stores index

--- app/routes/stores/index.tsx
+++ app/routes/stores/index.tsx
@@ -1,39 +1,54 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import type { Query } from "firebase-admin/firestore";
 import { auth } from "~/auth.server";
 import type { AuthUser } from "~/auth.server/auth-types";
 import { db } from "~/firebase";
 
+type StoreType = {
+  name: string;
+};
+
+const getStores = async (userId: string) => {
+  const storesRef = db.collection("users").doc(userId).collection("stores");
+  let query: Query = storesRef;
+  query = query.orderBy("name", "desc");
+
+  const stores = (await query.get()).docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+  return stores;
+};
+
 export const loader: LoaderFunction = async ({ request }) => {
   const user: AuthUser = await auth.user(request);
-
-  const userRef = await db.collection("users").doc(user.id);
-  const doc = await userRef.get();
-  if (!doc.exists) {
-    return null;
-  }
-
-  const { stores } = doc.data() || {};
+  const stores = await getStores(user.id);
   return {
-    user: await auth.user(request),
-    stores: stores,
+    user,
+    stores,
   };
 };
 
 export default function StoresIndexRoute() {
-  const { user, stores } = useLoaderData()
-  const storesNames = Object.keys(stores) 
+  const data = useLoaderData();
+  const user = data.user;
+  const stores: Array<StoreType> = data?.stores;
+
   return (
     <div>
       <h1 className="text-center pt-8 mb-4 text-4xl font-bold tracking-tight leading-none dark:text-white">
         Stores de {user.username} 📚
       </h1>
       <main className="flex flex-col">
+        {/* Crear componente lista de stores */}
         <ul className="flex flex-col mb-4 gap-2">
-          {!storesNames.length && <h1>"Debes crear una lista"</h1>}
-          {storesNames.map((list, index) => (
-            <li key={index} className="dark:text-white self-center bg-gray-700 p-2">
-              <Link to={list}>{list.toString()}</Link>
+          {!stores.length && <h1>No has creado ninguna lista</h1>}
+          {stores.map((store, index: any) => (
+            <li
+              key={index}
+              className="dark:text-white self-center bg-gray-700 p-2"
+            >
+              <Link to={store.name}>{store.name}</Link>
             </li>
           ))}
         </ul>
