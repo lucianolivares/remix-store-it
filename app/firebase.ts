b7683110c3b5363794bc7e6c1298024ff13c36a1
lucianolivares <lucianolivares@gmail.com>
2023-03-25 16:40:46 -0300

    refactor google key

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,30 +1,41 @@
-import { cert, initializeApp, getApps, getApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
+import {
+  initializeApp,
+  getApps,
+  getApp,
+  applicationDefault,
+} from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
-import type { App, ServiceAccount } from 'firebase-admin/app';
-import type { Auth } from 'firebase-admin/auth';
-import type { Firestore } from 'firebase-admin/firestore';
+import type { App } from "firebase-admin/app";
+import type { Auth } from "firebase-admin/auth";
+import type { Firestore } from "firebase-admin/firestore";
 
 let app: App;
 let auth: Auth;
-let restApiSignInUrl = '';
+let restApiSignInUrl = "";
 let db: Firestore;
 
-if (process.env.NODE_ENV === 'development') {
-  app = getApps().length === 0 ? initializeApp({ projectId: 'demo-storeit-f04dd' }) : getApp();
+if (process.env.NODE_ENV === "development") {
+  app =
+    getApps().length === 0
+      ? initializeApp({ projectId: "demo-storeit-f04dd" })
+      : getApp();
   restApiSignInUrl = `http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=123`;
 } else {
-  if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY === undefined) {
-    throw Error('Missing FIREBASE_SERVICE_ACCOUNT_KEY');
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS === undefined) {
+    throw Error("Missing GOOGLE_APPLICATION_CREDENTIALS");
   }
 
   if (process.env.FIREBASE_WEB_API_KEY === undefined) {
-    throw Error('Missing FIREBASE_WEB_API_KEY');
+    throw Error("Missing FIREBASE_WEB_API_KEY");
   }
-
-  const serviceAccount: ServiceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-  app = getApps().length === 0 ? initializeApp({ credential: cert(serviceAccount) }) : getApp();
+  app =
+    getApps().length === 0
+      ? initializeApp({
+          credential: applicationDefault(),
+        })
+      : getApp();
   restApiSignInUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_WEB_API_KEY}`;
 }
 
