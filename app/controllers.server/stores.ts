ed793c4e5347f553c6aa794c1bd276ba84ec770e
lucianolivares <lucianolivares@gmail.com>
2023-03-25 00:24:48 -0300

    move stores helpers to controller

--- app/controllers.server/stores.ts
+++ app/controllers.server/stores.ts
@@ -1,6 +1,17 @@
 import type { Query } from "firebase-admin/firestore";
 import { db } from "~/firebase";
 
+export type StoreType = {
+  name: string;
+};
+
+export type ProductType = {
+  name: string;
+  stock: string;
+  minStock: string;
+};
+
+
 export const getStoreId = async (userId:string, storeName:string) => {
   const storesRef = db.collection("users").doc(userId).collection("stores")
   let query = storesRef.where('name', '==', storeName);
@@ -12,10 +23,22 @@ export const getStoreId = async (userId:string, storeName:string) => {
   return store.id
 }
 
+export const getStores = async (userId: string) => {
+  const storesRef = db.collection("users").doc(userId).collection("stores");
+  let query: Query = storesRef;
+  query = query.orderBy("name", "desc");
+
+  const stores = (await query.get()).docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+  return stores;
+};
+
 export const getProductId = async (userId:string, storeId:string, productName:string) => {
   const productsRef = db.collection("users").doc(userId).collection("stores").doc(storeId).collection('products')
   let query = productsRef.where('name', '==', productName);
   const snapshot = await query.get()
+  console.log(productName)
   if (snapshot.empty){
     return ''
   }
