9bb2269b6f8214e9b28210c20f8db0ca040ac0ec
lucianolivares <lucianolivares@gmail.com>
2023-03-25 00:58:44 -0300

    styling storeslist

--- app/components/StoresList.tsx
+++ app/components/StoresList.tsx
@@ -3,10 +3,10 @@ import type { StoreType } from "~/controllers.server/stores";
 
 export default function StoresList({ stores }: { stores: Array<StoreType> }) {
   return (
-    <ul className="flex flex-col mb-4 gap-2">
+    <ul className="grid grid-cols-1 gap-2 m-4 text-gray-200 mx-auto">
       {!stores.length && <h1>No has creado ninguna lista</h1>}
       {stores.map((store, index: any) => (
-        <li key={index} className="dark:text-white self-center bg-gray-600 p-2">
+        <li key={index} className="flex justify-center px-4 py-2 bg-gray-700 rounded-md">
           <Link to={store.name}>{store.name}</Link>
         </li>
       ))}
