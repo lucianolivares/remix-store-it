df113f8217b2424db3f5e27289c9d2b4469b4465
lucianolivares <lucianolivares@gmail.com>
2023-03-25 00:25:04 -0300

    clean index route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,17 @@
 export default function IndexRoute() {
   return (
     <main className="bg-white dark:bg-gray-900 h-screen">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+        <div className="flex flex-col gap-4 justify-center mr-auto place-self-center px-4 py-8 mx-auto">
+          <h1 className="mx-auto max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none dark:text-white">
             📦 Store it !
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="mx-auto max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             Ten siempre a mano tu lista del mercado
             
           </p>
           <a
             href="/auth"
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            className="mx-auto inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
           >
             Iniciar
             <svg
@@ -30,18 +29,11 @@ export default function IndexRoute() {
           </a>
           <a
             href="/"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            className="mx-auto inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Ver Ejemplos
           </a>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-            alt="mockup"
-          />
-        </div>
-      </div>
     </main>
   );
 }
